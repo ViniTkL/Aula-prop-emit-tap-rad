e1e6d5db2b2e205f9a3e195fac3fcd9d64d2b602
unknown <ra189649@ACAD.UCDB.BR>
2024-04-24 22:21:40 -0400

    terceiro commit

---
+++ vue-project/src/components/Formulario.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import {ref} from 'vue';
+    const emit = defineEmits(['salvar-pessoa'])
+
+    const nome = ref('');
+    const cpf = ref('');
+    const categoria = ref('');
+
+    const onSubmit = () => {
+        emit('salvar-pessoa', nome.value, cpf.value, categoria.value)
+    }
+</script>
+<template>
+    <form @submit.prevent="onSubmit">
+        <div>
+            <label for="nome">Nome</label>
+            <input type="text" id="nome" v-model="nome">
+        </div>
+        <div>
+            <label for="CPF">CPF</label>
+            <input type="text" id="CPF" v-model="cpf">
+        </div>
+        <div>
+            <label for="CATEGORIA">CATEGORIA</label>
+            <input type="text" id="CATEGORIA" v-model="categoria">
+        </div>
+      
+
+        <button>Salvar</button>
+    </form>
+</template>
